4c818aa743ceac3779de06ec64f6e65e948b4156
Francisco Orona <oronafrankie87@gmail.com>
2023-05-07 16:42:16 -0600

    finished index.js

--- index.js
+++ index.js
@@ -3,27 +3,39 @@
 
 
 const questions = [
-    {
-      type: 'list',
-      name: 'shape',
-      message: `Let's build a logo! What type of shape do you want as the base?`,
-      choices: ['Circle', 'Triangle', 'Square'],
+  {
+    type: "list",
+    name: "shape",
+    message: `Let's build a logo! What type of shape do you want as the base?`,
+    choices: ["Circle", "Triangle", "Square"],
+  },
+  {
+    type: "list",
+    name: "color",
+    message: "What color do you want the shape to be?",
+    choices: ["Red", "Green", "Blue", "Yellow"],
+  },
+  {
+    type: "input",
+    name: "Text",
+    message:
+      "What words do you want within the shape? (Just press enter if none.)",
+    validate: (input) => {
+      const maxLength = 15;
+      if (input.length > maxLength) {
+        return `Text must be ${maxLength} characters or less (so it can easily fit within the shape).`;
+      }
+      return true;
     },
-    {
-      type: 'list',
-      name: 'color',
-      message: 'What color do you want the shape to be?',
-      choices: ['Red', 'Green', 'Blue', 'Yellow'],
-    },
-    {
-      type: 'input',
-      name: 'Text',
-      message: 'What words do you want within the shape? (Just press enter if none.)',
-      validate: (input) => {
-        const maxLength = 15
-        if (input.length > maxLength) {
-          return `Text must be ${maxLength} characters or less (so it can easily fit within the shape).`;
-        }
-        return true;
-      },
-    },
+  },
+  {
+    type: "list",
+    name: "textColor",
+    message: "What color do you want the text to be?",
+    choices: ["Red", "Green", "Blue", "Yellow", "White", "Black"],
+  },
+];
+
+async function init() {
+  var userDefinedLogoParams = await inquirer.prompt(questions);
+}
