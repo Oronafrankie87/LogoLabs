4c22887c2a87830af323ac5fdeda48a9b2d4fc83
Francisco Orona <oronafrankie87@gmail.com>
2023-05-07 17:29:23 -0600

    shapes.js added

--- index.js
+++ index.js
@@ -6,24 +6,24 @@ const questions = [
   {
     type: "list",
     name: "shape",
-    message: `Let's build a logo! What type of shape do you want as the base?`,
+    message: `Welcome to LogoLabs, for starters what shape do you want for your background?`,
     choices: ["Circle", "Triangle", "Square"],
   },
   {
     type: "list",
     name: "color",
-    message: "What color do you want the shape to be?",
+    message: "What color would you like for your shape?",
     choices: ["Red", "Green", "Blue", "Yellow"],
   },
   {
     type: "input",
     name: "Text",
     message:
-      "What words do you want within the shape? (Just press enter if none.)",
+      "Whats the name of your brand or company?",
     validate: (input) => {
-      const maxLength = 15;
+      const maxLength = 20;
       if (input.length > maxLength) {
-        return `Text must be ${maxLength} characters or less (so it can easily fit within the shape).`;
+        return `Text must be ${maxLength} characters or less to fit over the shape.`;
       }
       return true;
     },
@@ -31,7 +31,7 @@ const questions = [
   {
     type: "list",
     name: "textColor",
-    message: "What color do you want the text to be?",
+    message: "What color would you like the text to be?",
     choices: ["Red", "Green", "Blue", "Yellow", "White", "Black"],
   },
 ];
